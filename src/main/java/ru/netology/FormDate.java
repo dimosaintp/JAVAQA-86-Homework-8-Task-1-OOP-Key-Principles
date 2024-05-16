0ea94d4dec51e312db703993077a5b3694c81eab
Dmitrii Odynets <158339965+dimosaintp@users.noreply.github.com>
2024-05-16 21:55:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+}
